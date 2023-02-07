e26a5ab82c457da8d538d9fddfec1bec146c1f11
Naman Garg <90495241+namangarg-23@users.noreply.github.com>
2023-02-07 11:29:58 +0000

    Added a previous entries functionality

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 
 let count = 0
 
-function increment(){
+function increment() {
     count++;
-    document.getElementById("count-ele").innerText=count
-    
+    document.getElementById("count-ele").innerText = count
+
 }
-function decrease(){
-    if(count!=0){
+function decrease() {
+    if (count != 0) {
         count--;
     }
-    document.getElementById("count-ele").innerText=count
+    document.getElementById("count-ele").innerText = count
+}
+
+let prev = document.getElementById("prev")
+let temp = ""
+function save() {
+    temp += count + " - "
+    prev.innerText = temp
+    // prev.textContent=temp
 }
